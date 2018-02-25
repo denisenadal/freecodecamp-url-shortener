b8fa9d0800d267a49051f52012c69a31c0e17515
Denise Nadal <denisenadal@outlook.com>
2018-02-24 22:51:03 -0700

    works for encoded and unencoded routes now

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var urlObjSchema = mongoose.Schema({
 	originalUrl:{
 		type: String,
 		required: true,
-		default:"http://glitch.com"
+		default:"https://den-url-shortener.glitch.me"
 	},
 	shortUrl: String
 });
@@ -29,11 +29,9 @@ urlObjSchema.statics.findUrlObj = function(shortUrl,success,fail){
 	UrlObj.findOne({"shortUrl":shortUrl},
 		function(err,urlObj){
 			if(err){
-				console.log(err);
 				fail();
 			}
 			else{
-				console.log(urlObj);
 				success(urlObj);
 			}
 		});
@@ -64,32 +62,33 @@ app.get('/',function(req,res){
 	res.status(200).end('home');
 });
 
-//path for creating a new link
-app.get('/new/:url',function(req,res){
-	if(!validUrl.isUri(req.params.url) ){
-		res.status(400).json({err:"that doesn't look like a url"});
+app.get(/\/new\/https?(:\/\/)?.*/,function(req,res){
+	var requestedURL = req.url.replace('/new/','');
+	requestedURL = decodeURIComponent(requestedURL);
+	if(!validUrl.isWebUri(requestedURL) ){
+		res.status(400).json({err:"that doesn't look like a url"}).end();
 	}
-	var random = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
-	UrlObj.makeUrlObj(req.params.url,random, function(urlObj){
+	var random = "/"+Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+	UrlObj.makeUrlObj(requestedURL,random, function(urlObj){
 		//success
+		urlObj.shortUrl = req.protocol + '://'+req.hostname+urlObj.shortUrl;
 		res.status(200).json( urlObj);
 	},function(err){
 		//fail
 		res.status(500).json( {'err':err});
 	})
-
 });
 
 //any other url
 app.get('/*',function(req,res){
 	//if it's not the right size for a shortened url, reject it
-	if(req._parsedUrl.path.substr(1).length !== 5 ){
+	if(req._parsedUrl.path.length !== 6 ){
 		res.status(400).json({err:"that's not the right length. Sure you have the right url?'"});
 	}
 	else{
 		//else look for it in the database, if found, redirect user
-		UrlObj.findUrlObj(req._parsedUrl.path.substr(1), function(urlObj){
-			res.redirect(302,UrlObj.originalUrl);
+		UrlObj.findUrlObj(req.originalUrl, function(urlObj){
+			res.redirect(302,urlObj.originalUrl);
 		},function(err){
 			res.status(500).json( {'err':err});
 		});
